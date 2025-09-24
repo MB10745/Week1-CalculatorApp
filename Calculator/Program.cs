e41e076916c8899dd36e8d71d3f6f119ee474ffc
Mahnoor Baig <Baig-M4@ulster.ac.uk>
2025-09-24 14:17:16 +0300

    Wrote a program that adds two numbers and gives the result of that calculation as an output

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type in the first number followed by Enter key: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in the second number followed by Enter key: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} + {1} = {2}", firstNumber,secondNumber,result);
