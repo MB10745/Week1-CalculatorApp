3bffc187b30781f755f9dd273242681b007515d1
Mahnoor Baig <Baig-M4@ulster.ac.uk>
2025-09-24 14:32:50 +0300

    Updated Calculator program to do Addition, Subtraction, Multiplication & Division with numbers that user inputs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,9 +1,48 @@
-﻿Console.WriteLine("Type in the first number followed by Enter key: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+﻿CalculatorApp(); 
+void CalculatorApp()
+{
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
 
-Console.WriteLine("Type in the second number followed by Enter key: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Type in the first number followed by Enter key: ");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
 
-int result = firstNumber + secondNumber;
+    Console.WriteLine("Type in the second number followed by Enter key: ");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Adding {0} + {1} = {2}", firstNumber,secondNumber,result);
+    Console.WriteLine("Choose an option from the following list:");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Divide");
+    Console.WriteLine("4 - Multiply");
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1)
+    {
+        result = firstNumber + secondNumber;
+        Console.WriteLine($"Adding {firstNumber} + {secondNumber} = {result}");
+    }
+    else if (choice == 2)
+    {
+        result = firstNumber - secondNumber;
+        Console.WriteLine($"Subtracting {firstNumber} - {secondNumber} = {result}");
+    }
+    else if (choice == 3)
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"Dividing {firstNumber} / {secondNumber} = {result}");
+    }
+    else if (choice == 4)
+    {
+        result = firstNumber * secondNumber;
+        Console.WriteLine($"Multiplying {firstNumber} * {secondNumber} = {result}");
+    }
+    else
+    {
+        Console.WriteLine("You Did NOT Select A Number 1-4");
+    }
+
+    
+}
